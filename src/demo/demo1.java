daf9bdbe0a8cc7031b65bcc8deec079e0ffb8970
sodel <sodel@DESKTOP-GM6HDK0>
2025-06-05 03:08:38 -0700

    commit2

--- src/demo/demo1.java
+++ src/demo/demo1.java
@@ -1,7 +1,21 @@
 package demo;
 
+ class Dog {
+    void bark() {
+        System.out.println("The dog barks");
+    }
+}
+
+class Animal extends Dog {
+    void speak() {
+        System.out.println("All animals speak");
+    }
+}
+
 public class demo1 {
-	public static void main(String[] arg) {
-		System.out.println("hello world");
-	}
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        a.bark();  // Inherited from Dog
+        a.speak(); // Defined in Animal
+    }
 }
