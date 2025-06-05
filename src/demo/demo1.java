a00e8e3c529f541ab0db7fae9cb7ab9cc2ec1d45
Aryandudhankar <aryandudhankar1@gmail.com>
2025-06-05 04:29:24 -0700

    Update demo1.java

--- src/demo/demo1.java
+++ src/demo/demo1.java
@@ -15,7 +15,8 @@ class Animal extends Dog {
 public class demo1 {
     public static void main(String[] args) {
         Animal a = new Animal();
-        a.bark();  // Inherited from Dog
-        a.speak(); // Defined in Animal
+        a.bark(); 
+        a.speak(); 
+     System.out.println("git pull")
     }
 }
